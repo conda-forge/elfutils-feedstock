e90919f5f1993ee5272cc45ed30531d956974cf8
Tim Snyder <snyder.tim@gmail.com>
2022-04-02 05:03:10 +0000

    enable patched tests
    test failures in 0.186 were due to a few things:
    * need cap_sys_ptrace to run some tests.  I patched these to skip
      and if we're unable to add cap_sys_ptrace to the docker cmdline
      I'll propose it upstream
    * build everything with -g because some of the dwarf stuff is tested
      by introspecting the elfutils.  strip stuff at install time
    * test-nlist is really fragile and draconian in how it treats CFLAGS
      just patch it out of the automake file (that's why I need to
      autoreconf now)
    * many of the tests won't compile cleanly with NDEBUG because there
      are many assertions that we probably want during testing.  The
      cost of assertions in the utilites is low enough that we should just
      leave them in them.  Also given the esoteric nature of this package,
      leaving the assertions in is probably a good idea.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 export LIBS="$(pkg-config --libs-only-l zlib) $LIBS"
 export LDFLAGS="$(pkg-config --libs-only-L zlib) -lrt $LDFLAGS"
-export CFLAGS="$(pkg-config --cflags zlib) -Wno-unused-but-set-variable -Wno-unused-variable -Wno-null-dereference $CFLAGS"
-./configure --prefix=$PREFIX --with-zlib --enable-libdebuginfod=dummy || (cat config.log && exit 1)
+# tests introspect debug information in some of the binaries built.  Build with -g and strip at install
+export CFLAGS="$(pkg-config --cflags zlib) -Wno-unused-but-set-variable -Wno-unused-variable -Wno-null-dereference $CFLAGS -g"
+# tests don't work with NDEBUG
+export CPPFLAGS="${CPPFLAGS/-DNDEBUG/}"
+autoreconf --install --force
+mkdir build && cd build
+../configure --prefix=$PREFIX --mandir=$PREFIX/man --enable-test-rpath --with-zlib --enable-libdebuginfod=dummy || (cat config.log && exit 1)
 make -j${CPU_COUNT}
 
-# Unfortunately some tests fail, so we can't run "make check" here.
-# This is probably due to this package being a very sensitive package.
-# I believe this happens because it is not ready to be packaged into
-# an environment such as conda where it will run in different OSes,
-# environments, etc.
-#
-# For example, when running the tests on my personal machine, using
-# the docker image provided by condaforge, 8 tests failed, while in
-# CircleCI, 4 tests failed.
+make check -j${CPU_COUNT}
 
-make install
+make install-strip
